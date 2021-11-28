53fcdef974915155223eafc2e1ca64d8e1347714
Fikri Al Hafizh <91872566+Hafizh0192@users.noreply.github.com>
2021-11-28 08:54:56 +0700

    Add files via upload

---
+++ Pre-test 2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std; 
+
+int main (){
+	int a = 5000023;
+	cout << a <<endl;
+	cout << sizeof(a);
+	
+	//a=nama 
+	//b=cicilan perbulan
+	//c=bunga perbulan 
+	//d=lama pinjaman 
+	
+	
+	
+	cout << a << "  Fikri alhafizh: " <<a<< endl;
+	cout << "cicilan perbulan: " <<(a)*(a/50)<<endl;
+	
+}
